41df3aa4667d688d209b9898a220bbf2c3921a63
Fredrik Essmander <fredriklg3@gmail.com>
2021-01-10 15:25:27 +0100

    Added Authenticat, oidc-client

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,11 @@
   <v-row justify="center" align="center">
     <v-banner width="100%">
       <template v-slot:actions>
-        <!-- <v-btn text color="primary">Create</v-btn> -->
+        <v-btn @click="login" color="primary">Login</v-btn>
+        <v-btn @click="logout" color="primary">Logout</v-btn>
+        <v-btn @click="api('test')" color="primary">Test</v-btn>
+        <p>{{ testLabel }}</p>
+        <v-select v-model="select" :items="items"></v-select>
         <v-btn class="mx-2" fab dark color="indigo" @click="dialog = true">
           <v-icon color="black">mdi-plus</v-icon>
         </v-btn>
@@ -11,8 +15,8 @@
 
     <v-col cols="12" sm="8" md="6">
       <v-list three-line subheader v-if="bookings">
-        <v-list-item v-for="b in bookings">
-          <v-list-item-content>
+        <v-list-item v-for="b in bookings" v-if="bookings">
+          <v-list-item-content v-if="bookings">
             <v-list-item-title>{{ b.projectName }}</v-list-item-title>
             <v-list-item-subtitle>
               <p>Start: {{ b.start }}</p>
@@ -52,6 +56,7 @@ import axios from "axios";
 import { mapState, mapActions, mapMutations } from "vuex";
 import { schemaStore, dataStore } from "~/store";
 import CreateBooking from "~/components/CreateBooking.vue";
+import { UserManager, WebStorageStateStore } from "oidc-client";
 
 export default {
   components: {
@@ -60,15 +65,66 @@ export default {
   computed: {
     ...mapState({
       bookings: (state) => state.bookings,
+      testLabel: (state) => state.message,
     }),
   },
   data: () => ({
     dialog: false,
+    items: ["Today", "This week", "This month"],
+    select: "Today",
+
+    userMgr: null,
+
+    // testLabel: "",
   }),
+  created() {
+    if (!process.server) {
+      this.userMgr = new UserManager({
+        authority: "https://localhost:5001",
+        client_id: "web-client",
+        redirect_uri: "http://localhost:3000",
+        response_type: "code",
+        scope: "openid profile IdentityServerApi",
+        post_logout_redirect_uri: "http://localhost:3000",
+        //silent_redirect_uri: "http://localhost:3000/",
+        userStore: new WebStorageStateStore({ store: window.localStorage }),
+      });
+
+      this.userMgr.getUser().then(user => {
+        if(user) {
+          console.log("User from Storage", user);
+          this.$axios.setToken(`Bearer ${user.access_token}`);
+        }
+      });
+
+      const { code, scope, session_state, state } = this.$route.query;
+      if (code && scope && session_state && state) {
+        this.userMgr.signinRedirectCallback().then((user) => {
+          console.log(user);
+          this.$axios.setToken(`Bearer ${user.access_token}`);
+          this.$router.push('/');
+          
+        });
+      }
+    }
+  },
   methods: {
     ...mapMutations(["reset"]),
+    ...mapActions(["getTest"]),
     closeDialog(arg) {
       this.dialog = arg;
+    },
+    login() {
+      return this.userMgr.signinRedirect();
+    },
+    logout() {
+      return this.userMgr.signoutRedirect();
+    },
+    api(x) {
+      return this.$axios.$get("https://localhost:5001/api/bookings/test").then(s =>{
+        console.log(s)
+        });
+      // await this.getTest().then((x) => (this.testLabel = x));
     },
   },
 };
--- store/index.js
+++ store/index.js
@@ -8,13 +8,14 @@
 
 
 import Axios from "axios";
+import { getDefaultSettings } from "http";
 import https from 'https'
 
 
 
-const agent = new https.Agent({  
+const agent = new https.Agent({
     rejectUnauthorized: false
-  })
+})
 
 
 const initState = () => ({
@@ -29,20 +30,29 @@ export const mutations = {
     },
     reset(state) {
         Object.assign(state, initState())
+    },
+
+    setMessage(state, message) {
+        state.message = message
     }
 }
 
 export const actions = {
-    async nuxtServerInit({commit}){
+    async nuxtServerInit({ commit }) {
         const bookings = (await Axios.get("https://localhost:5001/api/bookings", { httpsAgent: agent })).data;
         commit("setBookings", bookings);
     },
-    async fetchBookings({commit}){
+    async fetchBookings({ commit }) {
         const bookings = (await Axios.get("https://localhost:5001/api/bookings", { httpsAgent: agent })).data;
         commit("setBookings", bookings);
     },
-    async createBooking({commit, dispatch}, {booking}) {
+    async createBooking({ commit, dispatch }, { booking }) {
         await Axios.post("https://localhost:5001/api/bookings", booking, { httpsAgent: agent });
         await dispatch('fetchBookings');
+    },
+    async getTest({ commit }) {
+        //window.console.log("APA TEST");
+        const message = (await Axios.get("https://localhost:5001/api/bookings/test", { httpsAgent: agent })).data;
+        commit("setMessage", message);
     }
 }
